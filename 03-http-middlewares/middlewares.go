f2b2ff6c1439d87d1195248da69a3f69d7f5558e
Anderson Queiroz <contato@andersonq.eti.br>
2020-06-24 11:15:50 +0200

    wip

--- 03-http-middlewares/middlewares.go
+++ 03-http-middlewares/middlewares.go
@@ -1,7 +1,29 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/rs/zerolog"
+)
 
 func TrackingID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+}
+
+func LogRequest(logger zerolog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+			logger.Info().Msg("request finished")
+		})
+	}
+
+}
+
+func a() {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	lm := LogRequest(zerolog.Logger{})
+	hh := lm(h)
+
+	hh.ServeHTTP(nil, nil)
 }
